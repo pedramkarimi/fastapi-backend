8dc9e7a1dd4677f38877c3c6d7143b0665bf9dc6
Pedram Karimi <pedram.karimi1988@gmail.com>
2025-12-12 10:03:49 +0330

    (feat):venv implement virtual environment and env files

--- src/core/config.py
+++ src/core/config.py
@@ -1,12 +1,26 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+import os
+from pathlib import Path
+
+
+APP_ENV = os.getenv("APP_ENV", "dev")
+BASE_DIR = Path(__file__).resolve().parents[2]  
+ENV_FILE = BASE_DIR / f".env.{APP_ENV}"
+
+if not ENV_FILE.exists():
+    raise RuntimeError(f"Env file not found: {ENV_FILE}")
+
+
 
 class Settings(BaseSettings):
+    DEBUG: bool = False
+
     # POSGRESQL 
-    DB_HOST: str = "localhost"
-    DB_PORT: int = 5432
-    DB_NAME: str = "fastapi_db"
-    DB_USER: str = "fastapi_user"
-    DB_PASSWORD: str = "fastapi_password"
+    DB_HOST: str 
+    DB_PORT: int 
+    DB_NAME: str 
+    DB_USER: str 
+    DB_PASSWORD: str 
 
     @property
     def SQLALCHEMY_DATABASE_URL(self) -> str:
@@ -16,38 +30,43 @@ class Settings(BaseSettings):
         )
     
     # JWT
-    JWT_SECRET_KEY: str = "super-secret-key-change-this"
-    JWT_ALGORITHM: str = "HS256"
-    JWT_ACCESS_TOKEN_EXPIRE_HOURS: int = 24
+    JWT_SECRET_KEY: str 
+    JWT_ALGORITHM: str 
+    JWT_ACCESS_TOKEN_EXPIRE_HOURS: int 
 
     # REDIS
-    REDIS_HOST: str = "localhost"
-    REDIS_PORT: int = 6379
-    REDIS_DB: int = 0
-    REDIS_URL: str = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
+    REDIS_HOST: str 
+    REDIS_PORT: int 
+    REDIS_DB: int 
+
+    @property
+    def REDIS_URL(self) -> str:
+        return f"redis://{self.REDIS_HOST}:{self.REDIS_PORT}/{self.REDIS_DB}"
 
     # REDIS KEY CONFIG
-    LOGIN_MAX_ATTEMPTS_PER_EMAIL: int = 5
-    LOGIN_MAX_ATTEMPTS_PER_IP: int = 20
-    LOGIN_ATTEMPT_WINDOW_SECONDS: int = 15 * 60  # پنجره شمارش تلاش‌ها (۱۵ دقیقه)
-    LOGIN_LOCK_TTL_SECONDS: int = 15 * 60        # زمان قفل بودن (۱۵ دقیقه)
-    RATELIMIT_DEFAULT_MAX_REQUESTS: int = 100
-    RATELIMIT_DEFAULT_WINDOW_SECONDS: int = 60
-    CACHED_DATA_TTL: int = 60
+    LOGIN_MAX_ATTEMPTS_PER_EMAIL: int 
+    LOGIN_MAX_ATTEMPTS_PER_IP: int
+    LOGIN_ATTEMPT_WINDOW_SECONDS: int 
+    LOGIN_LOCK_TTL_SECONDS: int 
+    RATELIMIT_DEFAULT_MAX_REQUESTS: int 
+    RATELIMIT_DEFAULT_WINDOW_SECONDS: int
+    CACHED_DATA_TTL: int
 
 
     # RABBITMQ
-    RABBITMQ_HOST: str = "localhost"
-    RABBITMQ_PORT: int = 5672
-    RABBITMQ_USER: str = "fastapi_user"
-    RABBITMQ_PASSWORD: str = "fastapi_pass"
-    WELCOME_EMAIL_QUEUE: str = "email.welcome"
+    RABBITMQ_HOST: str 
+    RABBITMQ_PORT: int 
+    RABBITMQ_USER: str 
+    RABBITMQ_PASSWORD: str 
+    WELCOME_EMAIL_QUEUE: str
 
 
     # CORS
-    BACKEND_CORS_ORIGINS : list[str]=["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:8000"]
+    BACKEND_CORS_ORIGINS : list[str]
     
-    class Config:
-        env_file = ".env"   
+
+    model_config = SettingsConfigDict(
+        env_file=str(ENV_FILE)
+    )
 
 settings = Settings()
